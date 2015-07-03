6f8518f039e37201b591e987f77210b4664a1d58
Marek Aufart <maufart@redhat.com>
2015-07-03 16:48:39 +0200

    adding EPEL to install pip on RHEL

--- rdosettler.sh
+++ rdosettler.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+# EPEL (needed for python-pip on RHEL)
+curl -O http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+sudo rpm -Uvh epel-release-7*.rpm
+
 # RPMs
 sudo yum -y install libffi-devel python-pip vim mc htop fuse-sshfs
 
